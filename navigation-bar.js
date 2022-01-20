1279daf6c815a549c5631407d930faf72fc8dcc9
Erdi Muliqi <erdieroni@gmail.com>
2022-01-20 19:37:28 +0100

    Website + Portfolio (portfolio in progress)

---
+++ navigation-bar.js
@@ -0,0 +1,16 @@
+(async () => {
+    const { pages } = await import("./pages.js");
+    const navBar = document.createElement("header");
+
+    navBar.className = "nav-bar";
+
+    for([pageName, href] of Object.entries(pages)) {
+        const element = document.createElement("a");
+        element.href = href;
+        element.innerText = pageName;
+
+        navBar.appendChild(element);
+    }
+
+    document.body.prepend(navBar);
+})();
---
+++ pages.js
@@ -0,0 +1,4 @@
+export const pages = {
+    "Home": "/",
+    "My Portfolio": "/pages/portfolio/portfolio.html"
+};
